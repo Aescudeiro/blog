58af470a43a0b91ad8c9af2016177df58ff0ca06
Afonso Escudeiro <aescudeiro@Afonsos-MBP.Home>
2022-12-12 20:32:12 -0100

    fix: fixed header search input width

--- apps/web/src/components/Header/components/Search/styles.ts
+++ apps/web/src/components/Header/components/Search/styles.ts
@@ -29,13 +29,11 @@ export const SearchIconWrapper = styled(Box)(({ theme }) => ({
 
 export const StyledInputBase = styled(InputBase)(({ theme }) => ({
   color: 'inherit',
+  width: '100%',
   '& .MuiInputBase-input': {
     padding: theme.spacing(1, 1, 1, 0),
     paddingLeft: `calc(1em + ${theme.spacing(4)})`,
     transition: theme.transitions.create('width'),
     width: '100%',
-    [theme.breakpoints.up('md')]: {
-      width: '20ch',
-    },
   },
 }));
